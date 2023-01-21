3cf4d6d602179be87fc55a01279384b9398cc5e0
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-21 01:24:49 +0100

    Added feedback message
    Added feedback message to show if a page or post was deleted.

--- admin/custom-functions/flush_cache_on_page_edit.php
+++ admin/custom-functions/flush_cache_on_page_edit.php
@@ -1,32 +1,42 @@
-<?php // Custom function - Add custom functions to flush cache on page edit
-
+<?php // Custom function - Add custom functions to flush cache on page or post edit
 
 // disable direct file access
-if (!defined('ABSPATH'))
-{
-
-    exit;
-
+if (!defined("ABSPATH")) {
+    exit();
 }
 
-
-$options = get_option('pressable_cache_management_options');
+$options = get_option("pressable_cache_management_options");
 
 // Get checkbox options and check if is not empty
-if (isset($options['flush_cache_page_edit_checkbox']) && !empty($options['flush_cache_page_edit_checkbox']))
-{
-
-    //Flush Batcache cache on page edit
-    function clear_batcache_on_post_edit()
+if (
+    isset($options["flush_cache_page_edit_checkbox"]) &&
+    !empty($options["flush_cache_page_edit_checkbox"])
+) {
+    function clear_batcache_on_post_edit($post_ID, $post, $update)
     {
-        wp_cache_flush();
+        if ($post->post_type === "post" || $post->post_type === "page") {
+            wp_cache_flush();
 
-        // Save time stamp to database if cache is flushed when a post or page was updated.
-        $object_cache_flush_time = date(' jS F Y  g:ia') . "\nUTC";
-        update_option('flush-cache-page-edit-time-stamp', $object_cache_flush_time);
-
-
+            if ($post->post_type === "post") {
+                $object_cache_flush_time_post =
+                    date(" jS F Y  g:ia") .
+                    "\nUTC" .
+                    "<b> — cache flushed due to post edit</b>";
+                update_option(
+                    "flush-cache-page-edit-time-stamp",
+                    $object_cache_flush_time_post
+                );
+            } elseif ($post->post_type === "page") {
+                $object_cache_flush_time_page =
+                    date(" jS F Y  g:ia") .
+                    "\nUTC" .
+                    "<b> — cache flushed due to page edit</b>";
+                update_option(
+                    "flush-cache-page-edit-time-stamp",
+                    $object_cache_flush_time_page
+                );
+            }
+        }
     }
-    add_action('save_post', 'clear_batcache_on_post_edit');
-
- }
+    add_action("save_post", "clear_batcache_on_post_edit", 10, 3);
+}
