68a39a19b975422a917e2af9959333623e34cfc2
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-26 17:25:57 +0100

    Update cdn_exclude_specific_file.php

--- admin/custom-functions/cdn_exclude_specific_file.php
+++ admin/custom-functions/cdn_exclude_specific_file.php
@@ -9,35 +9,51 @@ Author URI: https://pressable.com
 */
 
 if (!defined('IS_PRESSABLE'))
-    {
-        return;
-    }
+{
+    return;
+}
 
+function pcm_exclude_single_file($output)
+{
 
-    function pcm_exclude_single_file($output)
-    {
-        
-    //Set variable as default to prevent variable not defined error 
+    //Set variable as default to prevent variable not defined error
     if (!isset($excluded_file))
     {
         $excluded_file = '';
     }
-          
-        // $excluded_file = get_option('excluded_particular_file');
-        $excluded_file = preg_replace('/\s+/', '', $excluded_file);
-        stripslashes(rtrim($excluded_file,  '/'));
 
-        $output = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(' . get_option('excluded_particular_file') .')/i', $_SERVER['SERVER_NAME'] . '/$1$2', $output);
-        return $output;
-    }
+    // $excluded_file = get_option('excluded_particular_file');
+    $excluded_file = preg_replace('/\s+/', '', $excluded_file);
+    stripslashes(rtrim($excluded_file, '/'));
 
-    function pcm_custom_template_redirect()
+    $extend_cdn = "extend_cdn";
+    $excluded_file = get_option('excluded_particular_file');
+    $site_domain_url = $_SERVER['SERVER_NAME'];
+
+    if (empty($excluded_file))
     {
-        ob_start();
-        ob_start('pcm_exclude_single_file');
-        ob_flush();
+        return;
+    }
+    else
+    {
+		//Remove instances of extend_cdn query string to exempt files from cdn once it is excluded from CDN
+        $output = preg_replace('/((?!pressablecdn.com)(' . $excluded_file . ')[^"]*)(\?[^"]*)/i', '$2', $output);
+		
+        $output = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(' . get_option('excluded_particular_file') . ')/i', $_SERVER['SERVER_NAME'] . '/$1$2', $output);
+        return $output;
+
     }
 
-    // Grab the site contents and prep for the search/replace
-    add_action('template_redirect', 'pcm_custom_template_redirect');
-    
+    return $output;
+
+}
+
+function pcm_custom_template_redirect()
+{
+    ob_start();
+    ob_start('pcm_exclude_single_file');
+    ob_flush();
+}
+
+// Grab the site contents and prep for the search/replace
+add_action('template_redirect', 'pcm_custom_template_redirect');
