21c09928de876ca97aaa4b740328bf63a27f686a
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-28 09:52:37 +0100

    Moved mu-plugins
    Moved mu-plugins to a dedicated pressable-cache-management directory

--- admin/custom-functions/extend_batcache.php
+++ admin/custom-functions/extend_batcache.php
@@ -1,4 +1,4 @@
-<?php // Pressable Cache Management  - Extend batcache by 24 hours
+<?php // Pressable Cache Management  - Exctend batcache by 24 hours
 
 // disable direct file access
 if (!defined('ABSPATH'))
@@ -19,8 +19,23 @@ if (isset($options['extend_batcache_checkbox']) && !empty($options['extend_batca
     //Declear variable so that it can be accessed from cdn_exclude_specific_file.php
     $extend_batcache = get_option('extend_batcache_checkbox');
 
+    //Create the pressable-cache-management mu-plugin index file
+    $pcm_mu_plugins_index = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management.php';
+    if (!file_exists($pcm_mu_plugins_index))
+    {
+        // Copy pressable-cache-management.php from plugin directory to mu-plugins directory
+        copy(plugin_dir_path(__FILE__) . '/pressable_cache_management_mu_plugin_index.php', $pcm_mu_plugins_index);
+    }
+
+    // Check if the pressable-cache-management directory exists or create the folder
+    if (!file_exists(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/'))
+    {
+        //create the directory
+        wp_mkdir_p(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/');
+    }
+
     //Exclude specific files from CDN caching
-    $obj_extend_batcache = WP_CONTENT_DIR . '/mu-plugins/pcm_extend_batcache.php';
+    $obj_extend_batcache = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_extend_batcache.php';
     if (file_exists($obj_extend_batcache))
     {
 
@@ -28,8 +43,8 @@ if (isset($options['extend_batcache_checkbox']) && !empty($options['extend_batca
     else
     {
         $obj_extend_batcache = plugin_dir_path(__FILE__) . '/extend_batcache_mu_plugin.php';
-		
-        $obj_extend_batcache_active = WP_CONTENT_DIR . '/mu-plugins/pcm_extend_batcache.php';
+
+        $obj_extend_batcache_active = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_extend_batcache.php';
 
         if (!copy($obj_extend_batcache, $obj_extend_batcache_active))
         {
@@ -40,9 +55,8 @@ if (isset($options['extend_batcache_checkbox']) && !empty($options['extend_batca
 
         }
     }
-	
-	
-	  //Display admin notice
+
+    //Display admin notice
     function extend_batcache_admin_notice($message = '', $classes = 'notice-success')
     {
 
@@ -69,7 +83,7 @@ if (isset($options['extend_batcache_checkbox']) && !empty($options['extend_batca
                 if ($screen->id !== 'toplevel_page_pressable_cache_management') return;
 
                 $user = $GLOBALS['current_user'];
-				$message = sprintf('<p>Extending Batcache for 24 hours see <a href="https://pressable.com/knowledgebase/modifying-cache-times-batcache/"> Modifying Batcache Times.</a>', $user->display_name);
+                $message = sprintf('<p>Extending Batcache for 24 hours see <a href="https://pressable.com/knowledgebase/modifying-cache-times-batcache/"> Modifying Batcache Times.</a>');
 
                 extend_batcache_admin_notice($message, 'notice notice-success is-dismissible');
             });
@@ -80,18 +94,15 @@ if (isset($options['extend_batcache_checkbox']) && !empty($options['extend_batca
     }
     add_action('init', 'pcm_extend_batcache_admin_notice');
 
-	
-	
 }
 else
 {
-	
-	
+
     /**Update option from the database if the connection is deactivated
      used by admin notice to display and remove notice**/
     update_option('extend_batcache_activate_notice', 'activating');
-	
-    $obj_extend_batcache = WP_CONTENT_DIR . '/mu-plugins/pcm_extend_batcache.php';
+
+    $obj_extend_batcache = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_extend_batcache.php';
     if (file_exists($obj_extend_batcache))
     {
         unlink($obj_extend_batcache);
