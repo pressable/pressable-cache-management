1182d6a88e8a990c826457b021168f305a0c5335
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-28 09:54:57 +0100

    Moved mu-plugins
    Moved mu-plugins to a dedicated pressable-cache-management directory

--- admin/custom-functions/exclude_particular_file_from_cdn.php
+++ admin/custom-functions/exclude_particular_file_from_cdn.php
@@ -13,7 +13,20 @@ $options = get_option('cdn_settings_tab_options');
 if (isset($options['exclude_particular_file_from_cdn']) && !empty($options['exclude_particular_file_from_cdn']))
 {
 
+    //Create the pressable-cache-management mu-plugin index file
+    $pcm_mu_plugins_index = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management.php';
+    if (!file_exists($pcm_mu_plugins_index))
+    {
+        // Copy pressable-cache-management.php from plugin directory to mu-plugins directory
+        copy(plugin_dir_path(__FILE__) . '/pressable_cache_management_mu_plugin.php', $pcm_mu_plugins_index);
+    }
 
+    // Check if the pressable-cache-management directory exists or create the folder
+    if (!file_exists(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/'))
+    {
+        //create the directory
+        wp_mkdir_p(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/');
+    }
 
     //Add the option from the textbox into the database
     update_option('excluded_particular_file', $options['exclude_particular_file_from_cdn']);
@@ -21,11 +34,8 @@ if (isset($options['exclude_particular_file_from_cdn']) && !empty($options['excl
     //Declear variable so that it can be accessed from cdn_exclude_specific_file.php
     $excluded_file = get_option('excluded_particular_file');
 
-  
-
-
     //Exclude specific files from CDN caching
-    $cdn_exclude_specific_file = WP_CONTENT_DIR . '/mu-plugins/cdn_exclude_specific_file.php';
+    $cdn_exclude_specific_file = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/cdn_exclude_specific_file.php';
     if (file_exists($cdn_exclude_specific_file))
     {
 
@@ -33,7 +43,7 @@ if (isset($options['exclude_particular_file_from_cdn']) && !empty($options['excl
     else
     {
         $cdn_exclude_specific_file = plugin_dir_path(__FILE__) . '/cdn_exclude_specific_file.php';
-        $cdn_exclude_specific_file_active = WP_CONTENT_DIR . '/mu-plugins/cdn_exclude_specific_file.php';
+        $cdn_exclude_specific_file_active = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/cdn_exclude_specific_file.php';
 
         if (!copy($cdn_exclude_specific_file, $cdn_exclude_specific_file_active))
         {
@@ -48,7 +58,7 @@ if (isset($options['exclude_particular_file_from_cdn']) && !empty($options['excl
 }
 else
 {
-    $cdn_exclude_specific_file = WP_CONTENT_DIR . '/mu-plugins/cdn_exclude_specific_file.php';
+    $cdn_exclude_specific_file = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/cdn_exclude_specific_file.php';
     if (file_exists($cdn_exclude_specific_file))
     {
         unlink($cdn_exclude_specific_file);
