6a5ac07a380df3d14621b5286f2b48121a507352
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-28 09:58:33 +0100

    Moved mu-plugins
    Moved mu-plugins to a dedicated pressable-cache-management directory

--- admin/custom-functions/exclude_font_files_from_cdn.php
+++ admin/custom-functions/exclude_font_files_from_cdn.php
@@ -14,29 +14,44 @@ $options = get_option('cdn_settings_tab_options');
 if (isset($options['exclude_font_files_from_cdn']) && !empty($options['exclude_font_files_from_cdn']))
 {
 
+    //Create the pressable-cache-management mu-plugin index file
+    $pcm_mu_plugins_index = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management.php';
+    if (!file_exists($pcm_mu_plugins_index))
+    {
+        // Copy pressable-cache-management.php from plugin directory to mu-plugins directory
+        copy(plugin_dir_path(__FILE__) . '/pressable_cache_management_mu_plugin.php', $pcm_mu_plugins_index);
+    }
+
+    // Check if the pressable-cache-management directory exists or create the folder
+    if (!file_exists(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/'))
+    {
+        //create the directory
+        wp_mkdir_p(WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/');
+    }
 
     //Add the option from the textbox into the database
     update_option('exclude_font_files', $options['exclude_font_files_from_cdn']);
 
-
     //Exclude .jpg .png .webp from CDN caching
-    $cdn_exclude_font_files = WP_CONTENT_DIR . '/mu-plugins/pcm_exclude_font_files_from_cdn.php';
-    if (file_exists($cdn_exclude_font_files)) { 
+    $cdn_exclude_font_files = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_exclude_font_files_from_cdn.php';
+    if (file_exists($cdn_exclude_font_files))
+    {
 
-    } else {
-        $cdn_exclude_font_files = plugin_dir_path(__FILE__) . '/cdn_exclude_font_files_mu_plugins.php';
-        $cdn_exclude_font_files_active = WP_CONTENT_DIR . '/mu-plugins/pcm_exclude_font_files_from_cdn.php';
-
-         if(!copy($cdn_exclude_font_files,$cdn_exclude_font_files_active))
-         {
-
-         }
-         else
-         {
-
-         }
     }
-    
+    else
+    {
+        $cdn_exclude_font_files = plugin_dir_path(__FILE__) . '/cdn_exclude_font_files_mu_plugins.php';
+        $cdn_exclude_font_files_active = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_exclude_font_files_from_cdn.php';
+
+        if (!copy($cdn_exclude_font_files, $cdn_exclude_font_files_active))
+        {
+
+        }
+        else
+        {
+
+        }
+    }
 
     //Display admin notice
     function exclude_font_from_cdn_admin_notice($message = '', $classes = 'notice-success')
@@ -65,7 +80,7 @@ if (isset($options['exclude_font_files_from_cdn']) && !empty($options['exclude_f
                 if ($screen->id !== 'toplevel_page_pressable_cache_management') return;
 
                 $user = $GLOBALS['current_user'];
-                $message = sprintf('<p>Excluded all .WOFF .WOFF2 .OTF .TTF .EOT from CDN caching.</p>', $user->display_name);
+                $message = sprintf('<p>Excluded all .WOFF .WOFF2 .OTF .TTF .EOT from CDN caching.</p>');
 
                 exclude_font_from_cdn_admin_notice($message, 'notice notice-success is-dismissible');
             });
@@ -85,10 +100,14 @@ else
      used by admin notice to display and remove notice**/
     update_option('exclude_fonts_from_cdn_activate_notice', 'activating');
 
-    $cdn_exclude_font_files = WP_CONTENT_DIR . '/mu-plugins/pcm_exclude_font_files_from_cdn.php';
-    if (file_exists($cdn_exclude_font_files)) {
+    $cdn_exclude_font_files = WP_CONTENT_DIR . '/mu-plugins/pressable-cache-management/pcm_exclude_font_files_from_cdn.php';
+    if (file_exists($cdn_exclude_font_files))
+    {
         unlink($cdn_exclude_font_files);
-    } else {
+    }
+    else
+    {
         // File not found.
+        
     }
 }
