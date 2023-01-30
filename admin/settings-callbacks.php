246b5f963ff38ce033b8fd60c1c935c00499d45a
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-30 22:55:33 +0100

    View Batcache status
    Added features to view Batcache status

--- admin/settings-callbacks.php
+++ admin/settings-callbacks.php
@@ -32,7 +32,18 @@ function pressable_cache_management_callback_section_cache()
     }
 
     echo '<p>' . esc_html__('These settings enable you to manage the object cache.', 'pressable_cache_management') . '</p>';
+  
+	//Check for Batcache status on the site
+	$site_url = get_site_url();
+	$response = wp_remote_get( $site_url, array( 'timeout' => 120 ) );
 
+	if ( is_wp_error( $response ) || strpos( $response['body'], 'batcache' ) === false ) {
+
+	echo '<p style="text-align:right; font-weight:bold">Batcache Status: Broken &#128308;</p></br>';
+	} else {
+	    echo '<p style="text-align:right; font-weight:bold">Batcache Status: OK &#x1F7E2;</p>';		
+		}
+	}
 }
 
 function pressable_cache_management_callback_section_cdn()
