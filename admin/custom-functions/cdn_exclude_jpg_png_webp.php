7bfca2fa6ab4959a61085efd90ca27698e16eb51
tarhe <78844558+otarhe@users.noreply.github.com>
2023-02-01 19:15:38 +0100

    Update cdn_exclude_jpg_png_webp.php

--- admin/custom-functions/cdn_exclude_jpg_png_webp.php
+++ admin/custom-functions/cdn_exclude_jpg_png_webp.php
@@ -34,8 +34,6 @@ function pcm_jpg_png_webp_cdn_exempter($output)
     //Exclude all .image files from CDN
 	$output = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(.jpg|.jpeg|.png|.gif|.webp)/i', $_SERVER['SERVER_NAME'] . '$1$2', $output);
 
-	//Remove DB_NAME . '.v2.pressablecdn.com from img and srcset tags
-	$output = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com/i', $_SERVER['SERVER_NAME'], $output);
 
     //Remove instances of extend_cdn query string to exempt files from CDN once it is excluded from CDN
     $output = str_replace("?extend_cdn", "", $output);
