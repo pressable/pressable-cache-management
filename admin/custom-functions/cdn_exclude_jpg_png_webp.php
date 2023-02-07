99fe49486e991dd87933ca4e2d6447819db15e4f
tarhe <78844558+otarhe@users.noreply.github.com>
2023-02-07 18:17:35 +0100

    Fix bug
    Modified the code to remove the CDN URL for images inside the src and srcset attributes

--- admin/custom-functions/cdn_exclude_jpg_png_webp.php
+++ admin/custom-functions/cdn_exclude_jpg_png_webp.php
@@ -14,40 +14,49 @@ if (!defined('IS_PRESSABLE'))
     return;
 }
 
-function pcm_jpg_png_webp_cdn_exempter($output)
+function remove_query_string($html)
 {
-    //Commented out to prevent HTML Injection
-    // $output = html_entity_decode($output);
-    /*
-     * The line below replaces everything it finds before the comma (,) with what we define after it.
-     * In this case, that is replacing any CDN URL containing a specific file type extension or name with
-     * the site's own domain + path to the file (effectively exempting it from being served from the CDN).
-     *
-     * DB_NAME is used to grab the 9 digit value used for database names and each site's CDN URL.
-     * (.*) is a wildcard which allows this to work for files in any directory and with any name.
-     * (.json) is the file type or name you want to exclude, change to (.jpg) to exclude jpg files for example.
-     * For multiple file types, separate with a | e.g. (.jpg|.png|.css).
-     * To exclude a specific file, replace (.json) with (filename.ext) - e.g. (fusion-column-bg-image.js).
-     * $1$2 combines the values of (.*) and (.json) to form a complete file path (e.g. /wp-content/uploads/2021/02/file.json).
-    */
-
-    //Exclude all .image files from CDN
-	$output = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(.jpg|.jpeg|.png|.gif|.webp)/i', $_SERVER['SERVER_NAME'] . '$1$2', $output);
+	if (empty($html))
+	{
+	return;
+	}
 
 
-    //Remove instances of extend_cdn query string to exempt files from CDN once it is excluded from CDN
-    $output = str_replace("?extend_cdn", "", $output);
+  	$dom = new DOMDocument();
+	$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
 
-    //Return result for output
-    return $output;
+	$images = $dom->getElementsByTagName("img");
+	foreach ($images as $img) {
+    $src = $img->getAttribute("src");
+    $src = str_replace(DB_NAME . '.v2.pressablecdn.com', $_SERVER['SERVER_NAME'], $src);
+    $img->setAttribute("src", $src);
+
+    $srcset = $img->getAttribute("srcset");
+    if (!empty($srcset)) {
+        $srcset = str_replace(DB_NAME . '.v2.pressablecdn.com', $_SERVER['SERVER_NAME'], $srcset);
+        $img->setAttribute("srcset", $srcset);
+    }
 }
 
-function pcm_jpg_png_webp_cdn_template_redirect()
-{
-    ob_start();
-    ob_start('pcm_jpg_png_webp_cdn_exempter');
-    ob_flush();
+return $dom->saveHTML();
+
 }
 
-// Grab the site contents and prep for the search/replace
+function pcm_jpg_png_webp_cdn_exempter($html) {
+		
+	//$html = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(.jpg|.jpeg|.png|.gif|.webp)/i', $_SERVER['SERVER_NAME'] . '$1$2', $html);
+
+	//Removes the CDN URL from both the src and srcset attributes:
+	$html = remove_query_string($html);
+	
+  return $html;
+	
+}
+
+function pcm_jpg_png_webp_cdn_template_redirect() {
+  ob_start();
+  ob_start('pcm_jpg_png_webp_cdn_exempter');
+  ob_flush();
+}
+
 add_action('template_redirect', 'pcm_jpg_png_webp_cdn_template_redirect');
