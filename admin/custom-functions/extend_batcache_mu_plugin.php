62be15c1f898456622802c4de6d68ce73187eaaa
tarhe <78844558+otarhe@users.noreply.github.com>
2023-03-20 15:54:56 +0100

    Updated batcache code
    The Batcache code should be replaced with the following, ideally BEFORE the ‘require_once(ABSPATH . ‘wp-settings.php’);‘ line: inside the wp-content.php file

--- admin/custom-functions/extend_batcache_mu_plugin.php
+++ admin/custom-functions/extend_batcache_mu_plugin.php
@@ -5,10 +5,14 @@ if (!defined('IS_PRESSABLE'))
         return;
     }
 
-
-//Function to cache pages for 24 hours - useful for static site with less traffic 
+//Batcache Customizations
 global $batcache;
-if ( is_object($batcache) ) {
-     $batcache->max_age = 86400; // Seconds the cached render of a page will be stored
-     $batcache->seconds = 3600; // The amount of time at least 2 people are required to visit your page for a cached render to be stored.
+
+//Check is batcache params are in an object or an array, apply customizations accordingly
+if ( is_object( $batcache ) ) {
+    $batcache->max_age = 86400; // Seconds the cached render of a page will be stored
+    $batcache->seconds = 1200; // The amount of time at least 2 people are required to 
+} elseif ( is_array( $batcache ) ) {
+    $batcache['max_age'] = 86400; // Seconds the cached render of a page will be stored
+    $batcache['seconds'] = 1200;// The amount of time at least 2 people are required to 
 }
