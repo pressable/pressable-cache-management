0e1d602bf5b57212caf7efac468ffe908b141fdb
tarhe <78844558+otarhe@users.noreply.github.com>
2023-03-20 16:03:17 +0100

    Update pressable-cache-management.php

--- pressable-cache-management.php
+++ pressable-cache-management.php
@@ -4,7 +4,7 @@ Plugin Name:  Pressable Cache Management
 Description:  Presable cache management made easy
 Plugin URI:   https://pressable.com/knowledgebase/pressable-cache-management-plugin/#overview
 Author:       Pressable CS Team
-Version:      3.4.9
+Version:      3.3.10
 Requires at   least: 5.0
 Tested up to: 6.0
 Requires PHP: 7.4
