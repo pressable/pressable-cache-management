ede0300248e36008d14d60d600ecb93355adbb9a
tarhe <78844558+otarhe@users.noreply.github.com>
2023-06-07 22:20:49 +0100

    Update cdn_extender.php

--- admin/custom-functions/cdn_extender.php
+++ admin/custom-functions/cdn_extender.php
@@ -1,5 +1,6 @@
 <?php //Extend Pressable's cache-control from 7 days until 10 years for static assets
 
+
 // disable direct file access
 if (!defined('ABSPATH'))
 {
@@ -20,112 +21,116 @@ function pressablecdn_template_redirect()
  * and scripts of the theme.
  **********/
 
-function pcm_append_querystring_theme_scripts()
+// Only apply the modification to pages that are not RSS feed pages.
+if (strpos($_SERVER['REQUEST_URI'], '/feed/') === false)
 {
-    $extensions = array(
-        '.css',
-        '.js'
-    );
-    $styles = wp_styles();
-    foreach ($styles->registered as & $style)
+
+    function pcm_append_querystring_theme_scripts()
     {
-        if (is_null($style->src) || empty($style->src)) continue;
-        // check if the src path contains any of the extensions
-        foreach ($extensions as $ext)
+        $extensions = array(
+            '.css',
+            '.js'
+        );
+        $styles = wp_styles();
+        foreach ($styles->registered as & $style)
         {
-            if (strpos($style->src, $ext) !== false)
+            if (is_null($style->src) || empty($style->src)) continue;
+            // check if the src path contains any of the extensions
+            foreach ($extensions as $ext)
             {
-                $style->src = add_query_arg('extend_cdn', '', $style->src);
-                break;
+                if (strpos($style->src, $ext) !== false)
+                {
+                    $style->src = add_query_arg('extend_cdn', '', $style->src);
+                    break;
+                }
+            }
+        }
+
+        $scripts = wp_scripts();
+        foreach ($scripts->registered as & $script)
+        {
+            if (is_null($script->src) || empty($script->src)) continue;
+            // check if the src path contains any of the extensions
+            foreach ($extensions as $ext)
+            {
+                if (strpos($script->src, $ext) !== false)
+                {
+                    $script->src = add_query_arg('extend_cdn', '', $script->src);
+                    break;
+                }
             }
         }
     }
+    add_action('wp_enqueue_scripts', 'pcm_append_querystring_theme_scripts', PHP_INT_MAX);
 
-    $scripts = wp_scripts();
-    foreach ($scripts->registered as & $script)
+    /*********
+     * This code uses a regular expression to search for <img and src attributes in the HTML
+     * And appends the query string ?extend_cdn before the file extension of the src attribute
+     * value for <img> tags. The code only appends the query string to  URL's with pressablecdn.com
+     **********/
+
+    function extend_cache_for_images($html)
     {
-        if (is_null($script->src) || empty($script->src)) continue;
-        // check if the src path contains any of the extensions
-        foreach ($extensions as $ext)
+        if (empty($html))
         {
-            if (strpos($script->src, $ext) !== false)
+            return;
+        }
+
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument();
+        $dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+        $images = $dom->getElementsByTagName("img");
+        foreach ($images as $img)
+        {
+            $src = $img->getAttribute("src");
+            $img->setAttribute("src", $src . "?extend_cdn");
+            $srcset = $img->getAttribute("srcset");
+            if (!empty($srcset))
             {
-                $script->src = add_query_arg('extend_cdn', '', $script->src);
-                break;
+                $srcset = preg_replace("/(https?:\/\/[^\s]+)/", "$1?extend_cdn", $srcset);
+                $img->setAttribute("srcset", $srcset);
             }
         }
+        return $dom->saveHTML();
+
     }
-}
-add_action('wp_enqueue_scripts', 'pcm_append_querystring_theme_scripts', PHP_INT_MAX);
 
-/*********
- * This code uses a regular expression to search for <img and src attributes in the HTML
- * And appends the query string ?extend_cdn before the file extension of the src attribute
- * value for <img> tags. The code only appends the query string to  URL's with pressablecdn.com
- * The code searches for any <link> tags with href attributes containing certain font file types,
- * and appends the same query string "extend_cdn" to the value of the href attribute.
- **********/
+    //If CDN extender is enabled don't append query string to images files
+    $options = get_option('cdn_settings_tab_options');
 
-function extend_cache_for_images($html)
-{
-    if (empty($html))
+    if (isset($options['exclude_jpg_png_webp_from_cdn']) && !empty($options['exclude_jpg_png_webp_from_cdn']))
     {
         return;
     }
 
-    libxml_use_internal_errors(true);
-    $dom = new DOMDocument();
-    $dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-    libxml_clear_errors();
-    $images = $dom->getElementsByTagName("img");
-    foreach ($images as $img)
+    function pressablecdn_ob_call($html)
     {
-        $src = $img->getAttribute("src");
-        $img->setAttribute("src", $src . "?extend_cdn");
-        $srcset = $img->getAttribute("srcset");
-        if (!empty($srcset))
-        {
-            $srcset = preg_replace("/(https?:\/\/[^\s]+)/", "$1?extend_cdn", $srcset);
-            $img->setAttribute("srcset", $srcset);
-        }
-    }
 
-    $fonts = $dom->getElementsByTagName("link");
-    foreach ($fonts as $font)
-    {
-        $href = $font->getAttribute("href");
-        //Appending query string to font can potencially break a website due to the way fonts are added to the site
-        //         if (preg_match("/\.(eot|otf|svg|ttf|woff|woff2)$/i", $href)) {
+        $html = extend_cache_for_images($html);
+
+        /**********
+         * The code searches for any <link> tags with href attributes containing certain font file types,
+         * and appends the same query string "extend_cdn" to the value of the href attribute.
+         **********/
+
+        $html = preg_replace('/(<link[^>]+href[\s]*=[\s]*["\'])([^"\']+\.(eot|otf|svg|ttf|woff|woff2))(["\'])/i', '$1$2?extend_cdn$4', $html);
+        //$html = preg_replace('/(<link[^>]+href[\s]*=[\s]*["\'])(https?:\/\/' . DB_NAME . '.v2.pressablecdn.com\/)([^"\']+\.(eot|otf|svg|ttf|woff|woff2))(["\'])/i', '$1$2$3?extend_cdn$5', $html);
+        
+
         /******
-         * Dont append ?extend_cdn to these files jquery.json.min.js and gtm.js
-         * jquery.json.min.js: is used by Gravity forms it causes error when
-         * ?extend_cdn is appended to it gtm.js: is used by Google analytics it
-         * would throw 404 due to extend_cdn appended to it.
+         * Rename instances of jquery.js?extend_cdnon.min.js to
+         * jquery.json.min.js to fix Gravityform jquery.json.min.js
+         * file from 404'ing due to extend_cdn renianming it incorrectly.
          ****/
-        if (!in_array($href, ['gtm.js', 'jquery.json.min.js']))
-        {
-            $font->setAttribute("href", $href . "?extend_cdn");
-            //             }
-            
-        }
+        $html = str_replace("jquery.js?extend_cdnon.min.js", "jquery.json.min.js", $html);
+
+        /* Exclude Google Tag Manager gtm.js from Pressable CDN to fix Google tracking issue bug */
+        $html = str_replace("gtm.js?extend_cdn", "gtm.js", $html);
+
+        return $html;
     }
-    return $dom->saveHTML();
-}
 
-//If CDN extender is enabled don't append query string to images files
-$options = get_option('cdn_settings_tab_options');
-
-if (isset($options['exclude_jpg_png_webp_from_cdn']) && !empty($options['exclude_jpg_png_webp_from_cdn']))
-{
-    return;
-}
-
-function pressablecdn_ob_call($html)
-{
-
-    $html = extend_cache_for_images($html);
-
-    return $html;
 }
 
 if (!is_admin() && strpos($_SERVER['REQUEST_URI'], "wp-admin") === false && strpos($_SERVER['REQUEST_URI'], "wp-login.php") === false)
