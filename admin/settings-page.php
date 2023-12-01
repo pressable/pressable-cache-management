2af5793a16f051a510988271554cf6d9af11daac
tarhe <78844558+otarhe@users.noreply.github.com>
2023-12-01 18:41:09 +0100

    Update settings-page.php
    Fixed footer conflict with Yoursite plugin which causes  critical error

--- admin/settings-page.php
+++ admin/settings-page.php
@@ -23,41 +23,41 @@ function pressable_cache_management_display_settings_page()
 ?>
 
 
-	  <!-- Our admin page content should all be inside .wrap -->
+      <!-- Our admin page content should all be inside .wrap -->
 
-	  <?php
+      <?php
     $remove_pressable_branding_tab_options = get_option('remove_pressable_branding_tab_options');
 
 ?>
 
-		 <div class="wrap branding-<?php echo (is_array($remove_pressable_branding_tab_options)) ? esc_html(json_encode($remove_pressable_branding_tab_options)) : esc_html($remove_pressable_branding_tab_options); ?>">
+         <div class="wrap branding-<?php echo (is_array($remove_pressable_branding_tab_options)) ? esc_html(json_encode($remove_pressable_branding_tab_options)) : esc_html($remove_pressable_branding_tab_options); ?>">
 
 
 
-		<!-- Print the page title -->
-		<h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <!-- Print the page title -->
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
 
-		<!-- Here are our tabs -->
-		<nav class="nav-tab-wrapper">
-		  <a href="admin.php?page=pressable_cache_management" class="nav-tab nav-tab-object-cache <?php if ($tab === null): ?>nav-tab-active<?php
+        <!-- Here are our tabs -->
+        <nav class="nav-tab-wrapper">
+          <a href="admin.php?page=pressable_cache_management" class="nav-tab nav-tab-object-cache <?php if ($tab === null): ?>nav-tab-active<?php
     endif; ?>">Object Cache</a>
-		  <a href="admin.php?page=pressable_cache_management&tab=cdn_settings_tab" class="nav-tab nav-tab-cdn <?php if ($tab === 'cdn_settings_tab'): ?>nav-tab-active<?php
+          <a href="admin.php?page=pressable_cache_management&tab=cdn_settings_tab" class="nav-tab nav-tab-cdn <?php if ($tab === 'cdn_settings_tab'): ?>nav-tab-active<?php
     endif; ?>">CDN</a>
-			<a href="admin.php?page=pressable_cache_management&tab=edge_cache_settings_tab" class="nav-tab nav-tab-edge-cache <?php if ($tab === 'edge_cache_settings_tab'): ?>nav-tab-active<?php
+            <a href="admin.php?page=pressable_cache_management&tab=edge_cache_settings_tab" class="nav-tab nav-tab-edge-cache <?php if ($tab === 'edge_cache_settings_tab'): ?>nav-tab-active<?php
     endif; ?>">Edge Cache(Beta)</a>
-		  <a href="admin.php?page=pressable_cache_management&tab=pressable_api_authentication_tab" class="nav-tab nav-tab-api <?php if ($tab === 'pressable_api_authentication_tab'): ?>nav-tab-active<?php
+          <a href="admin.php?page=pressable_cache_management&tab=pressable_api_authentication_tab" class="nav-tab nav-tab-api <?php if ($tab === 'pressable_api_authentication_tab'): ?>nav-tab-active<?php
     endif; ?>">API Authentication</a>
 
-		<!-- Hidden from the plugin view can be accessed from the love icon below the plugin footer -->
-		  <a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" class="nav-tab nav-tab-hidden <?php if ($tab === 'remove_pressable_branding_tab'): ?>nav-tab-active<?php
+        <!-- Hidden from the plugin view can be accessed from the love icon below the plugin footer -->
+          <a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" class="nav-tab nav-tab-hidden <?php if ($tab === 'remove_pressable_branding_tab'): ?>nav-tab-active<?php
     endif; ?>">Hidden Tab Remove Branding</a>
-		</nav>
+        </nav>
 
-	  <!-- Switch between tabs -->
+      <!-- Switch between tabs -->
 
-	  <div class="wrap">
-		<h1 ><?php echo esc_html(get_admin_page_title()); ?></h1>
-		<form action="options.php" method="post"><?php
+      <div class="wrap">
+        <h1 ><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <form action="options.php" method="post"><?php
     if ($default_tab == $tab)
     {
 
@@ -138,114 +138,61 @@ function pressable_cache_management_display_settings_page()
 ?>
 
 
-	<style type="text/css">
+    <style type="text/css">
 
-	/** Hide Pressable branding tab **/
-	.nav-tab-hidden,
-	#footer-built-with-love.branding-disable,
-	.branding-disable h2,
-	.branding-disable .pressablecmlogo { display:none!important; }
+    /** Hide Pressable branding tab **/
+    .nav-tab-hidden,
+    #footer-built-with-love.branding-disable,
+    .branding-disable h2,
+    .branding-disable .pressablecmlogo { display:none!important; }
 
-	</style>
-	  </div><?php
+    </style>
+      </div><?php
 }
 
-//Display footer message with Pressable branding
+// Display footer message with Pressable branding
 function pcm_footer_msg()
 {
-
     /**
      **********************************************************
      * Add remove pressable branding_tab_options table to DB if
-     * not exist  to always enable Pressable pranding by default
+     * not exist to always enable Pressable branding by default
      **********************************************************
      */
-
     if ('not-exists' === get_option('remove_pressable_branding_tab_options', 'not-exists'))
     {
 
-        //Add the options table if it does not exist
+        // Add the options table if it does not exist
         add_option('remove_pressable_branding_tab_options', '');
 
-        //Set opions table to enable default
+        // Set options table to enable default
         $pcm_enable_pressable_branding = array(
             'branding_on_off_radio_button' => 'enable',
         );
 
         update_option('remove_pressable_branding_tab_options', $pcm_enable_pressable_branding);
-
     }
 
-    add_action('admin_notices', function ()
-    {
-
-        $screen = get_current_screen();
-
-        //Display footer message for only Pressable Cache Management plugin page
-        if ($screen->id !== 'toplevel_page_pressable_cache_management') return;
-
-        $user = $GLOBALS['current_user'];
-
-        //Show Pressable footer branding
-        function remove_footer_admin()
-        {
-
-            $remove_pressable_branding_tab_options = false;
-
-            $remove_pressable_branding_tab_options = get_option('remove_pressable_branding_tab_options');
-
-            if ($remove_pressable_branding_tab_options && 'enable' == $remove_pressable_branding_tab_options['branding_on_off_radio_button'])
-            {
-
-                echo '<span id="footer-built-with-love" class="branding-';
-                echo $remove_pressable_branding_tab_options['branding_on_off_radio_button'];
-                echo '">Built with 
-	<a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" style="text-decoration: none; color: transparent;"><span class="heart" style="color:red; font-size:24px;">&#x2665;</span></a> by The Pressable CS Team.';
-
-            }
-        }
-
-        add_filter('admin_footer_text', 'remove_footer_admin');
-    });
-
+    // Hook to replace the default WordPress footer with your plugin's footer
+    add_filter('admin_footer_text', 'pcm_replace_default_footer');
 }
-add_action('init', 'pcm_footer_msg');
 
-//Display footer message without Pressable branding
-function pcm_footer_msg_remove_branding()
+// Function to replace the default WordPress footer with your plugin's footer
+function pcm_replace_default_footer()
 {
+    $remove_pressable_branding_tab_options = get_option('remove_pressable_branding_tab_options');
 
-    add_action('admin_notices', function ()
+    if ($remove_pressable_branding_tab_options && 'enable' == $remove_pressable_branding_tab_options['branding_on_off_radio_button'])
     {
-
-        $screen = get_current_screen();
-
-        //Display footer message for only Pressable Cache Management plugin page
-        if ($screen->id !== 'toplevel_page_pressable_cache_management') return;
-
-        $user = $GLOBALS['current_user'];
-
-        // Admin footer modification | Hide Pressable footer branding
-        function remove_pressable_footer_branding()
-        {
-
-            $remove_pressable_branding_tab_options = false;
-
-            $remove_pressable_branding_tab_options = get_option('remove_pressable_branding_tab_options');
-
-            if ($remove_pressable_branding_tab_options && 'disable' == $remove_pressable_branding_tab_options['branding_on_off_radio_button'])
-            {
-
-                echo '<span id="footer-built-with-love" class="branding-';
-                //             echo $remove_pressable_branding_tab_options['branding_on_off_radio_button'];
-                echo '">Built with 
-	<a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" style="text-decoration: none; color: transparent;"><span class="heart" style="color:red; font-size:24px;">&#x2665;</span></a>';
-
-            }
-        }
-
-        add_filter('admin_footer_text', 'remove_pressable_footer_branding');
-    });
-
+        echo '<span id="footer-thankyou">Built with 
+    <a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" style="text-decoration: none; color: transparent;"><span class="heart" style="color:red; font-size:24px;">&#x2665;</span></a> by The Pressable CS Team.</span>';
+    }
+    else
+    {
+        echo '<span id="footer-thankyou">Built with 
+    <a href="admin.php?page=pressable_cache_management&tab=remove_pressable_branding_tab" style="text-decoration: none; color: transparent;"><span class="heart" style="color:red; font-size:24px;">&#x2665;</span></a></span>';
+    }
 }
-add_action('init', 'pcm_footer_msg_remove_branding');
+
+// Hook functions to the admin_init action
+add_action('admin_init', 'pcm_footer_msg');
