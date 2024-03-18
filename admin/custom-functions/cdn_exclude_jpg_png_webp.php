00f75cd71c4223fc1e2b743a93050ae197ebcef0
tarhe <78844558+otarhe@users.noreply.github.com>
2024-03-18 22:19:09 +0300

    Update cdn_exclude_jpg_png_webp.php
    Fix warning error bug with DOMDocument.
    
    Updated the code from using DOMDocument to regular expressions to directly  modify the HTML to remove and re-add the CDN address from a URL

--- admin/custom-functions/cdn_exclude_jpg_png_webp.php
+++ admin/custom-functions/cdn_exclude_jpg_png_webp.php
@@ -1,62 +1,35 @@
-<?php //Plugin Name: Exempt Images .jpg, .jpeg, .png, .gif, .webp From CDN
+<?php
+// Plugin Name: Exempt Images .jpg, .jpeg, .png, .gif, .webp From CDN
 
-
-// disable direct file access
-if (!defined('ABSPATH'))
-{
-
+// Disable direct file access
+if (!defined('ABSPATH')) {
     exit;
-
 }
 
-if (!defined('IS_PRESSABLE'))
-{
+if (!defined('IS_PRESSABLE')) {
     return;
 }
 
-function remove_query_string($html)
-{
-	if (empty($html))
-	{
-	return;
-	}
-
-
-  	$dom = new DOMDocument();
-	$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-
-	$images = $dom->getElementsByTagName("img");
-	foreach ($images as $img) {
-    $src = $img->getAttribute("src");
-    $src = str_replace(DB_NAME . '.v2.pressablecdn.com', $_SERVER['SERVER_NAME'], $src);
-    $img->setAttribute("src", $src);
-
-    $srcset = $img->getAttribute("srcset");
-    if (!empty($srcset)) {
-        $srcset = str_replace(DB_NAME . '.v2.pressablecdn.com', $_SERVER['SERVER_NAME'], $srcset);
-        $img->setAttribute("srcset", $srcset);
-    }
-}
-
-return $dom->saveHTML();
-
-}
-
 function pcm_jpg_png_webp_cdn_exempter($html) {
-		
-	//$html = preg_replace('/' . DB_NAME . '.v2.pressablecdn.com(.*)(.jpg|.jpeg|.png|.gif|.webp)/i', $_SERVER['SERVER_NAME'] . '$1$2', $html);
+    // Define the CDN URL and server name
+    $cdn_url = DB_NAME . '.v2.pressablecdn.com';
+    $server_name = $_SERVER['SERVER_NAME'];
 
-	//Removes the CDN URL from both the src and srcset attributes:
-	$html = remove_query_string($html);
-	
-  return $html;
-	
+    // Define an array of image extensions
+    $image_extensions = array('.jpg', '.jpeg', '.png', '.gif', '.webp');
+
+    // Iterate over each image extension and replace CDN URLs
+    foreach ($image_extensions as $extension) {
+        $pattern = '/(' . preg_quote($cdn_url, '/') . ')(.*?' . preg_quote($extension, '/') . ')/i';
+        $replacement = $server_name . '$2';
+        $html = preg_replace($pattern, $replacement, $html);
+    }
+
+    return $html;
 }
 
 function pcm_jpg_png_webp_cdn_template_redirect() {
-  ob_start();
-  ob_start('pcm_jpg_png_webp_cdn_exempter');
-  ob_flush();
+    ob_start('pcm_jpg_png_webp_cdn_exempter');
 }
 
 add_action('template_redirect', 'pcm_jpg_png_webp_cdn_template_redirect');
