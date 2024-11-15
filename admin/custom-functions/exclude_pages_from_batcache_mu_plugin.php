709c61767035e2a24b881f3a835f778b2d3d249e
tarhe <78844558+otarhe@users.noreply.github.com>
2024-11-15 22:16:42 +0530

    Update exclude_pages_from_batcache_mu_plugin.php
    Added features to exclude home page from caching

--- admin/custom-functions/exclude_pages_from_batcache_mu_plugin.php
+++ admin/custom-functions/exclude_pages_from_batcache_mu_plugin.php
@@ -9,16 +9,19 @@ if (!defined('IS_PRESSABLE'))
 
 $options = get_option('pressable_cache_management_options');
 
-//Import options from the database
+// Import options from the database
 $exempted_pages = $options['exempt_from_batcache'];
 
-// Use explode to split the pages using comma
 if (function_exists('batcache_cancel')) {
-    $exempted_pages = explode(', ', $exempted_pages);
+    // Use explode to split the pages using a comma and trim spaces
+    $exempted_pages = array_map('trim', explode(',', $exempted_pages));
 
+    // Add a check for the homepage
     foreach ($exempted_pages as $page) {
-        if (strpos($_SERVER['REQUEST_URI'], $page) !== false) {
+        if (strpos($_SERVER['REQUEST_URI'], $page) !== false || $_SERVER['REQUEST_URI'] === '/') {
             batcache_cancel();
+            break; // Exit the loop as we've already canceled caching
         }
     }
 }
+
