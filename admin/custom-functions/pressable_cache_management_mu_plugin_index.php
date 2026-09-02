d761fd8ce29311dc7231131795b33a2e85c73c13
tarhe <78844558+otarhe@users.noreply.github.com>
2026-09-02 14:18:39 +0100

    Change require to require_once for cache management files

--- admin/custom-functions/pressable_cache_management_mu_plugin_index.php
+++ admin/custom-functions/pressable_cache_management_mu_plugin_index.php
@@ -18,31 +18,31 @@ if (!defined('ABSPATH'))
 
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_extend_batcache.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_extend_batcache.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_extend_batcache.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_specific_file.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_specific_file.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_specific_file.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cdn_extender.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cdn_extender.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cdn_extender.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_jpg_png_webp.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_jpg_png_webp.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_jpg_png_webp.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_css.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_css.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_css.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_js_json.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_js_json.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/cdn_exclude_js_json.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_font_files_from_cdn.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_font_files_from_cdn.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_font_files_from_cdn.php';
 }
 
 // if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/batcache_manager.php')) {
@@ -50,15 +50,15 @@ require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_font_files_from
 // }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cache_wpp_cookies_pages.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cache_wpp_cookies_pages.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_cache_wpp_cookies_pages.php';
 }
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_query_string_gclid.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_query_string_gclid.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_query_string_gclid.php';
 }
 
 
 
 if(file_exists(WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_pages_from_batcache.php')) {
-require WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_pages_from_batcache.php';
+require_once WPMU_PLUGIN_DIR.'/pressable-cache-management/pcm_exclude_pages_from_batcache.php';
 }
